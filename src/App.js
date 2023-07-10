27e88007d18a7549f81a064ff9bed4607c217530
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-10 12:02:00 -0700

    Replaces images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
+// import About from "./pages/About.js";
+// import Portfolio from "./pages/Portfolio.js";
+// import Contact from "./pages/Contact.js";
+// import Resume from "./pages/Resume.js";
+
+// import Career from "./pages/Career.js";
+// import Education from "./pages/Education";
+// import FooterApp from "./components/Footer.js";
+
 
 function App() {
   return (
