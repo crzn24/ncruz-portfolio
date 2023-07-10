a46d5d1da04b93370949ca072537ced7e2cc7409
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-10 14:19:46 -0700

    Cleans up App.js to debug Navbar not displaying. Npm i react icons.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import "./index.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import About from "./pages/About.js";
-// import Portfolio from "./pages/Portfolio.js";
-// import Contact from "./pages/Contact.js";
-// import Resume from "./pages/Resume.js";
-
-// import Career from "./pages/Career.js";
-// import Education from "./pages/Education";
-// import FooterApp from "./components/Footer.js";
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Navbar />
     </div>
   );
 }
