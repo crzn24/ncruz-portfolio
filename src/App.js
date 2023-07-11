7695100c5ca03eb283517914f8ba5fbb65ff0d91
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-11 14:48:37 -0700

    Imports skills componenet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar"; // can click control + spacebar to autoimport when filling in div with components below
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Skills />
     </div>
   );
 }
