82e8a9e1559edb9b8966ce9f9c3859fd70399dcd
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-12 14:27:21 -0700

    Adds comment and changes name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 
+// need name='home' for react smooth scroll on Navbar
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,7 +13,7 @@ const Work = () => {
     <div name='work' className='w-full md:h-screen bg-[#0a192f] text-gray-300'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Work</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Portfolio</p>
                 <p className='py-6'>// Check out some of my recent work</p>
             </div>
 
