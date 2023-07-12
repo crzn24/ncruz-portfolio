76a7980a21e87c775d3ea3bf6e0b356a28f1d307
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-12 14:40:54 -0700

    Fixes bug that didn't close mobile hamburger menu after selecting an item. Adds onClick={handleClick} to Link tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
           </li>
           <li>
             <Link to="about" smooth={true} duration={500}>
-              About
+              About Me
             </Link>
           </li>
           <li>
@@ -57,11 +57,31 @@ const Navbar = () => {
       {/* mobile menu */}
         {/* ternary operator */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About Me</li>
-        <li className='py-6 text-4xl'>Portfolio</li>
-        <li className='py-6 text-4xl'>Contact</li>
-        <li className='py-6 text-4xl'>Resume</li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About Me
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Portfolio
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to="resume" smooth={true} duration={500}>
+            Resume
+          </Link>
+        </li>
       </ul>
 
       {/* social icons */}
