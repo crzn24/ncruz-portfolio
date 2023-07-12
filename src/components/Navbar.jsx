46af8ffda9f2580245793775066052577506e1d4
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-12 14:47:37 -0700

    Adds links for GitHub and LinkedIn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -90,13 +90,13 @@ const Navbar = () => {
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href='/'>
+            href='https://www.linkedin.com/in/nathancruz-/'>
               LinkedIn <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a className='flex justify-between items-center w-full text-gray-300'
-            href='/'>
+            href='https://github.com/crzn24'>
               GitHub <FaGithub size={30} />
             </a>
           </li>
