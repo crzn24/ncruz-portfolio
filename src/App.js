c12ea1bee965dd06c422173efe85a0630285d8c5
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-12 13:00:03 -0700

    Starts Contact form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar"; // can click control + spacebar to autoimport when filling in div with components below
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Skills />
       <Work />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// need to add name= for React smooth scroll 
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center p-4'>
+        <form action='' className='flex flex-col max-w-[600px] w-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
+                <p className='py-4 text-gray-300 '>Submit the form below or send me an email - @gmail.com</p>
+            </div>
+            <input type='text' placeholder='Name' name='name' className='p-2 bg-[#ccd6f6]'/>
+            <input type='email' placeholder='Email' name='email' className='my-4 p-2 bg-[#ccd6f6]'/>
+            <textarea placeholder='Message' name='message' rows="10" className='p-2 bg-[#ccd6f6]'></textarea>
+            <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center'>Let's Collaborate</button>
+        </form>
+    </div>
+  )
+}
+
+export default Contact
