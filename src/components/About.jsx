21c8723dd1df59c24e920c51b4e816977bb5402a
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-14 12:27:53 -0700

    Adds some different styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,18 +13,40 @@ const About = () => {
 
           <div></div>
 
-        
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          {/* was px-4 */}
+          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8">
             <div className="sm:text-right text-4xl font-bold">
-                <p>Hi! I'm Nathan, nice to meet you. Please take a look around.</p>
+              <p>
+                Hi! I'm <span className="text-[#574271]">Nathan</span>, nice to meet you. Please take a look around.
+              </p>
             </div>
-            
-          
-          <div>
-            <p>I am passionate about building software that... I specialize in creating...</p>
-          </div>
-        </div>
 
+            <div>
+              <p>
+                I'm an elementary school educator who has embarked on an
+                exciting journey into the world of web development. Beyond my
+                passion for education, I find joy in sports, fitness, hiking,
+                Star Wars, gaming, and all forms of art and creative expression.
+                I thrive on solving problems through unconventional thinking and
+                have a strong desire to build software that enhances user
+                experiences.
+              </p>
+            </div>
+          </div>
+
+          <div className="max-w-[1000px] w-full grid sm:grid-cols-2">
+            <div>
+              <p>
+                With a focus on creating interactive and intuitive
+                web applications, I aim to merge functionality with aesthetics to
+                make a positive impact in the digital realm, promoting
+                accessibility and inclusivity. Join me on this exciting journey
+                as we bring innovative ideas to life and empower individuals
+                through technology. Together, let's create a better and more
+                inclusive digital landscape!"
+              </p>
+            </div>
+          </div>
 
 
         </div>
