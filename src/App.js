2b30743ced393fca14b60daa61e2200740fe2a69
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-07-14 11:13:34 -0700

    Creates resume component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar"; // can click control + spacebar to autoimport when filling in div with components below
+import Resume from "./components/Resume";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
 
@@ -14,6 +15,7 @@ function App() {
       <Skills />
       <Work />
       <Contact />
+      <Resume />
     </div>
   );
 }
---
+++ src/components/Resume.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div name="resume" className="w-full h-screen bg-[#1D1E1F] text-gray-300">
+        <div className="flex flex-col justify-center items-center w-full h-full">
+            <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+                <div className="sm:text-right pb-8 pl-4">
+                    <p className="text-4xl font-bold inline border-b-4 border-[#FADE4B]">
+                        Coming Soon...
+                    </p>
+            </div>
+
+            {/* <div></div> */}
+
+                {/* <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+                    <div className="sm:text-right text-4xl font-bold">
+                        <p>
+                            Hi! I'm Nathan, nice to meet you. Please take a look around.
+                        </p>
+                    </div>
+
+                <div>
+                    <p>
+                        I am passionate about building software that... I specialize in
+                        creating...
+                    </p>
+                </div>
+                </div> */}
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Resume;
