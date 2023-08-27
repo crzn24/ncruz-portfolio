d91f7e9e53e719a7d986243447ee60333906fe52
Nathan Cruz <113571118+crzn24@users.noreply.github.com>
2023-08-27 13:37:08 -0700

    Fixes typo in Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <form method="POST" action='https://getform.io/f/6edc435c-3bbd-45a0-8b12-dbb62e93bc6b' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#FADE4B] text-gray-300'>Contact</p>
-                <p className='py-4 text-gray-300 '>Thanks for stopping by! I built this site to to showcase my proficiency in leveraging the following technologies: HTML, CSS, JavaScript, React, TailwindCSS. This site is also mobile friendly!</p>
+                <p className='py-4 text-gray-300 '>Thanks for stopping by! I built this site to showcase my proficiency in leveraging the following technologies: HTML, CSS, JavaScript, React, TailwindCSS. This site is also mobile friendly!</p>
                 <p className='py-4 text-gray-300 '>For any professional inquiries you can fill out the form below or shoot me an email - <a href="mailto:cruzn@uw.edu"> cruzn@uw.edu</a></p>
             </div>
             <input type='text' placeholder='Name' name='name' className='p-2 bg-[#ccd6f6]'/>
